9f90438f2912b77d7357f8c0eb27d62bf5639169
mmmatthew <matthew.mdv@gmail.com>
2018-02-11 23:38:21 +0100

    select fountain in list

--- code/src/app/list/list.component.ts
+++ code/src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
+import {NgRedux} from 'ng2-redux';
+import {IAppState} from '../store';
+import {SELECT_FOUNTAIN} from '../actions';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +12,16 @@ import {DataService} from '../data.service';
 export class ListComponent implements OnInit {
   public fountains;
 
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, private ngRedux: NgRedux<IAppState>) {
 
   }
 
   ngOnInit() {
 
+  }
+
+  public selectFountain(id){
+    this.ngRedux.dispatch({type:SELECT_FOUNTAIN, fountainId: id});
   }
 
 }
