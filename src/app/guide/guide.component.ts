c5520ec970cf19fa02be38bb20c65085c273af6e
mmmatthew <matthew.mdv@gmail.com>
2018-08-18 11:13:41 +0200

    templated contribution guides

--- src/app/guide/guide.component.ts
+++ src/app/guide/guide.component.ts
@@ -1,41 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
 import {select} from '@angular-redux/store';
-// import {GuideSelectorComponent} from '../guide-selector/guide-selector.component';
-
-// @Component({
-//   selector: 'app-guide',
-//   styleUrls: ['./guide.component.css'],
-//   template: ''
-// })
-// export class GuideComponent extends GuideSelectorComponent implements OnInit {
-//   @select('fountainSelected') fountain;
-//
-//   constructor( ) { }
-//
-//   ngOnInit() {
-//   }
-//   //
-//   // openGuide(guideName):void{
-//   //   switch(guideName){
-//   //     case 'image': this.bottomSheet.open(ImageGuideComponent);
-//   //     case 'gallery': this.bottomSheet.open(GalleryGuideComponent);
-//   //   }
-//   //
-//   // }
-//
-// }
 
 
 @Component({
   selector: 'app-guide-selector',
-  styleUrls: ['./guide-selector.component.css'],
+  styleUrls: ['./guide.component.css'],
   templateUrl: './guide-selector.component.html'
 })
 export class GuideSelectorComponent implements OnInit {
   @select('fountainSelected') fountain;
 
-  constructor( private guidRef: MatBottomSheetRef<GuideSelectorComponent>,
+  constructor( private bottomSheetRef: MatBottomSheetRef<GuideSelectorComponent>,
                private bottomSheet: MatBottomSheet) { }
 
   ngOnInit() {
@@ -47,13 +23,17 @@ export class GuideSelectorComponent implements OnInit {
       case 'image': {this.bottomSheet.open(ImageGuideComponent); break;}
       case 'gallery': {this.bottomSheet.open(GalleryGuideComponent); break;}
     }
+  }
 
+  closeGuide():void{
+    this.bottomSheetRef.dismiss()
   }
 }
 
 
 @Component({
   selector: 'app-image-guide',
+  styleUrls: ['./guide.component.css'],
   templateUrl: './image.guide.component.html',
 })
 export class ImageGuideComponent extends GuideSelectorComponent {}
@@ -62,6 +42,7 @@ export class ImageGuideComponent extends GuideSelectorComponent {}
 
 @Component({
   selector: 'app-gallery-guide',
+  styleUrls: ['./guide.component.css'],
   templateUrl: './gallery.guide.component.html',
 })
 export class GalleryGuideComponent extends GuideSelectorComponent {}
