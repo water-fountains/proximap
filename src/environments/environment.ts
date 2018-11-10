c943069dcd0b26e00853848896bf5f5b1cbe3cdb
mmmatthew <matthew.mdv@gmail.com>
2018-11-10 15:43:17 +0100

    minor refactorization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  ApiUrlBeta: '//localhost:3000/',
-  ApiUrlStable: '//localhost:3001/',
+  apiUrlBeta: '//localhost:3000/',
+  apiUrlStable: '//localhost:3001/',
   mapboxApiKey: 'pk.eyJ1Ijoid2F0ZXItZm91bnRhaW5zIiwiYSI6ImNqZGZ1cDR4bTA5OGcyeGxuamYzZnI2c20ifQ.aE2ji5z01HuLBGXNYraZYQ'
 };
