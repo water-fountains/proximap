4554b9231037ec8c3843d53634ce17229e020183
stanko <stanko@gmail.com>
2020-04-08 21:01:47 +0200

    Travis fi

--- fromDataBlue.js
+++ fromDataBlue.js
@@ -9,6 +9,28 @@ const apiUrlBeta = 'https://api.beta.water-fountains.org/';
 const apiUrlStable = 'https://api.water-fountains.org/';
 const apiUrlLocal = 'http://localhost:3000/'; // use in development.
 
+const callAPI = function(branch, filename, url) {
+  const apiUrl = (branch === 'stable') ? apiUrlStable : apiUrlBeta;
+
+  let metadataUrl = `${apiUrl}api/v1/metadata/${url}`;
+
+  // Get fountain_properties data from server and create file.
+  axios.get(metadataUrl)
+  .then(function (response) {
+    // handle success
+    const data = JSON.stringify(response.data, null, 4);
+    if (!fs.existsSync(filename)) {
+      fs.writeFileSync(filename, '', {encoding: 'utf8'});
+    }
+    fs.writeFileSync(filename, data, {encoding: 'utf8'});
+    console.log(`${url} sync DONE.`);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+}
+
 if (process.argv.length > 2) {
   process.argv.forEach(function (val, index, array) {
     if (val.indexOf('for') !== -1) {
@@ -16,26 +38,116 @@ if (process.argv.length > 2) {
       for (let arg of args) {
         switch(arg) {
           case 'fountains':
-            createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties');
+            createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
             break;
           case 'locations':
-            createSharedFile(`./src/assets/locations.json`, 'locations');
+            createSharedFile(`./src/assets/locations.json`, 'locations')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
             break;
           case 'constants':
-            createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants');
+            createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
             break;
           default:
-            createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties');
-            createSharedFile(`./src/assets/locations.json`, 'locations');
-            createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants');
+            createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
+            createSharedFile(`./src/assets/locations.json`, 'locations')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
+            createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants')
+              .then(
+                resp => {
+
+                }
+              )
+              .catch(
+                error => {
+                  console.error(error);
+                }
+              );
+            }
+          }
         }
-      }
-    }
-  })
-} else {
-  createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties');
-  createSharedFile(`./src/assets/locations.json`, 'locations');
-  createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants');
+      })
+    } else {
+      createSharedFile(`./src/assets/fountain_properties.json`, 'fountain_properties')
+        .then(
+          resp => {
+
+          }
+        )
+        .catch(
+          error => {
+            console.error(error);
+          }
+        );
+      createSharedFile(`./src/assets/locations.json`, 'locations')
+        .then(
+          resp => {
+
+          }
+        )
+        .catch(
+          error => {
+            console.error(error);
+          }
+        );
+      createSharedFile(`./src/assets/shared-constants.json`, 'shared-constants')
+        .then(
+          resp => {
+
+          }
+        )
+        .catch(
+          error => {
+            console.error(error);
+          }
+        );
 }
 
 
@@ -51,26 +163,4 @@ async function createSharedFile(filename, url) {
                  }
                }));
   callAPI(branch, filename, url)
-}
-
-const callAPI = function(branch, filename, url) {
-  const apiUrl = (branch === 'stable') ? apiUrlStable : apiUrlLocal;
-
-  let metadataUrl = `${apiUrl}api/v1/metadata/${url}`;
-
-  // Get fountain_properties data from server and create file.
-  axios.get(metadataUrl)
-  .then(function (response) {
-    // handle success
-    const data = JSON.stringify(response.data, null, 4);
-    if (!fs.existsSync(filename)) {
-      fs.writeFileSync(filename, '', {encoding: 'utf8'});
-    }
-    fs.writeFileSync(filename, data, {encoding: 'utf8'});
-    console.log(`${url} sync ${branch} DONE to `+filename+' '+new Date().toISOString());
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error+' '+new Date().toISOString());
-  });
 }
